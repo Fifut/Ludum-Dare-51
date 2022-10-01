596aa5c77ee751cd2623378ac5547c020bd6fd4a
Fabien <streit.fabien@gmail.com>
2022-10-01 18:24:23 +0200

    Sons

--- Scenes/Game/BreakableWall.gd
+++ Scenes/Game/BreakableWall.gd
@@ -6,6 +6,10 @@ onready var Dirt = preload("res://Scenes/Game/Dirt.tscn")
 onready var InteractArea = $"%InteractArea"
 onready var AnimatedSprite = $"%AnimatedSprite"
 onready var CollisionShape2D = $"%CollisionShape2D"
+onready var Hurt01 = $"%Hurt01"
+onready var Hurt02 = $"%Hurt02"
+onready var Hurt03 = $"%Hurt03"
+onready var Hurt04 = $"%Hurt04"
 
 
 
@@ -17,7 +21,15 @@ func _process(_delta):
 	if enable and AnimatedSprite.frame < 3 and Input.is_action_just_pressed("interact"):
 		AnimatedSprite.frame += 1
 		
+		match AnimatedSprite.frame:
+			1: Hurt01.play()
+			2: Hurt02.play()
+			3: Hurt03.play()
+		
 	elif enable and AnimatedSprite.frame >= 3 and Input.is_action_just_pressed("interact"):	
+		
+		Hurt04.play()
+
 		var dirt = Dirt.instance()
 		dirt.position = position
 		get_parent().add_child(dirt)
--- Scenes/Game/Toilet.gd
+++ Scenes/Game/Toilet.gd
@@ -5,6 +5,7 @@ extends StaticBody2D
 onready var DropArea = $"%DropArea"
 onready var Dirt = $"%Dirt"
 onready var Water = $"%Water"
+onready var Drop = $"%Drop"
 
 
 
@@ -23,6 +24,7 @@ func _process(_delta):
 		
 		if Input.is_action_just_pressed("grab") and body.has_method("drop") and not Dirt.visible:
 			if body.drop():
+				Drop.play()
 				Dirt.show()			
 			
 		if Input.is_action_just_pressed("interact") and Dirt.visible:
