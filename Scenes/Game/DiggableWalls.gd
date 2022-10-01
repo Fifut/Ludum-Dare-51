47f446b122b3094f2289341ecc465f5552c35a97
Fabien <streit.fabien@gmail.com>
2022-10-01 14:41:56 +0200

    Random murs a creuser

---
+++ Scenes/Game/DiggableWalls.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+onready var Unbreakable = preload("res://Scenes/Game/UnbreakableWall.tscn")
+onready var Breakable = preload("res://Scenes/Game/BreakableWall.tscn")
+
+
+
+
+func _ready():
+	
+	randomize()
+	
+	for x in range(0, 19):
+		for y in range(0, 12):
+			var breakable = Breakable.instance()
+			breakable.position = Vector2(120 + 60 * x, 60 + 60 * y)
+			add_child(breakable)
+		
+	for i in range(0, 10):
+		var unbreakable = Unbreakable.instance()
+		unbreakable.position = Vector2(120 + 60 * (randi() % 19), 60 + 60 * (randi() % 12) )
+		add_child(unbreakable)
+		
